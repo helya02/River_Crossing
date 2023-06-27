32077067752a4573f612394475049f7c2c0b1c2c
helya02 <77358433+helya02@users.noreply.github.com>
2023-06-27 19:21:01 +0330

    node.py

--- node.py
+++ node.py
@@ -1,8 +1,9 @@
 #class of node
-#with the help of this class, we can create a node that save:
+#with the help of this class, we can create a node that saves:
 #state: show On which side of the river are the people and the boat? .
-#state type: state structure is a python dictioanary. the key of dict is short name of peopele and boat , and the value of dict is show the location of people and boat. 0 is on the left side of river and 1 is on the right side of the river.
-#depth: show the depth of node in the DLS search tree.
+#state type: state structure is a Python dictionary. the key of dict is the short name of the people and boat ,
+#and the value of dict is show the location of the people and boat. 0 is on the left side of the river and 1 is on the right side of the river.
+#depth: show the depth of the node in the DLS search tree.
 #depth type: is an integer 
 #path: save a list of nodes that have been seen during the path to reach this node.
 #path type: a python list. and contains nodes.
